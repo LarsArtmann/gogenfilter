20354803e4474e63b189a7e5be61363e82168293
Lars Artmann <git@lars.software>
2026-05-26 00:49:53 +0200

    chore(infra): add flake.nix for website with Node.js, run nix fmt

--- website/flake.nix
+++ website/flake.nix
@@ -5,75 +5,97 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      apps = forAllSystems (system:
+      apps = forAllSystems (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
         {
           dev = {
             type = "app";
-            program = "${pkgs.writeShellApplication {
-              name = "dev";
-              runtimeInputs = with pkgs; [ nodejs ];
-              text = ''
-                npm run dev
-              '';
-            }}/bin/dev";
+            program = "${
+              pkgs.writeShellApplication {
+                name = "dev";
+                runtimeInputs = with pkgs; [ nodejs ];
+                text = ''
+                  npm run dev
+                '';
+              }
+            }/bin/dev";
           };
 
           build = {
             type = "app";
-            program = "${pkgs.writeShellApplication {
-              name = "build";
-              runtimeInputs = with pkgs; [ nodejs ];
-              text = ''
-                npm run build
-              '';
-            }}/bin/build";
+            program = "${
+              pkgs.writeShellApplication {
+                name = "build";
+                runtimeInputs = with pkgs; [ nodejs ];
+                text = ''
+                  npm run build
+                '';
+              }
+            }/bin/build";
           };
 
           preview = {
             type = "app";
-            program = "${pkgs.writeShellApplication {
-              name = "preview";
-              runtimeInputs = with pkgs; [ nodejs ];
-              text = ''
-                npm run preview
-              '';
-            }}/bin/preview";
+            program = "${
+              pkgs.writeShellApplication {
+                name = "preview";
+                runtimeInputs = with pkgs; [ nodejs ];
+                text = ''
+                  npm run preview
+                '';
+              }
+            }/bin/preview";
           };
 
           deploy = {
             type = "app";
-            program = "${pkgs.writeShellApplication {
-              name = "deploy";
-              runtimeInputs = with pkgs; [ nodejs firebase-tools ];
-              text = ''
-                npm run build
-                firebase deploy --only hosting
-              '';
-            }}/bin/deploy";
+            program = "${
+              pkgs.writeShellApplication {
+                name = "deploy";
+                runtimeInputs = with pkgs; [
+                  nodejs
+                  firebase-tools
+                ];
+                text = ''
+                  npm run build
+                  firebase deploy --only hosting
+                '';
+              }
+            }/bin/deploy";
           };
 
           validate-docs = {
             type = "app";
-            program = "${pkgs.writeShellApplication {
-              name = "validate-docs";
-              runtimeInputs = with pkgs; [ nodejs ];
-              text = ''
-                npm run validate:docs
-              '';
-            }}/bin/validate-docs";
+            program = "${
+              pkgs.writeShellApplication {
+                name = "validate-docs";
+                runtimeInputs = with pkgs; [ nodejs ];
+                text = ''
+                  npm run validate:docs
+                '';
+              }
+            }/bin/validate-docs";
           };
-        });
+        }
+      );
 
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
@@ -84,15 +106,20 @@
               firebase-tools
             ];
           };
-        });
+        }
+      );
 
-      checks = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
           format = pkgs.runCommand "format-check" { nativeBuildInputs = [ pkgs.nixfmt ]; } ''
             nixfmt --check ${./flake.nix} && touch $out
           '';
-        });
+        }
+      );
 
       formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
     };
